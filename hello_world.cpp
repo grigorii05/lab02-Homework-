074f5169f97b80f188722ada271e8efada933d30
grigorii05 <grisha5akimov98@mail.ru>
2019-04-01 18:46:28 +0300

    without using namespace std;

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,10 @@
 include <iostream>
-using namespace std;
 
 void main()
 {
  char N[15];
- cout<<"Enter your username: ";
- cin>>N;
- cout<<"Hello World "<<N;
+ std::cout<<"Enter your username: ";
+ std::cin>>N;
+ std::cout<<"Hello World "<<N;
 }
 # lab02-Homework-
