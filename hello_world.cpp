ce0346bf1e49d7803577fc000e44f5a3388a4d08
grigorii05 <grisha5akimov98@mail.ru>
2019-04-06 17:04:18 +0300

    update comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 void main()
 {
- char N[15];
- cout<<"Enter your username: ";
- cin>>N;
- cout<<"Hello World "<<N;
+ char N[15]; // English comments
+ cout<<"Enter your username: "; // Enter username
+ cin>>N;// N = username
+ cout<<"Hello World "<<N;// result
 }
 # lab02-Homework-
